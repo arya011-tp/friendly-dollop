5cee1631418b3f452ccdd58c022f8068f4cb8d9f
arya011-tp <arya.aniket@tyreplex.com>
2023-03-09 19:41:24 +0530

    sdufsdufvuodsuofousf

--- src/configs/config.js
+++ src/configs/config.js
@@ -1,11 +1,9 @@
 module.exports.Config = {
     NODE_ENV: process.env.NODE_ENV || 'development',
-    PORT: process.env.PORT || 3000,
-    DB_HOST: process.env.DB_HOST || '15.206.56.7',
-    DB_USER: process.env.DB_USER || 'rndshared',
-    DB_PASS: process.env.DB_PASS || 'PY9BwpFHHJnMehxd',
-    DB_DATABASE: process.env.DB_DATABASE || 'rndshared',
-    SECRET_JWT: process.env.SECRET_JWT || "",
-    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY || "SENDGRID_API_KEY",
-    SENDGRID_SENDER: process.env.SENDGRID_SENDER || "FROM_EMAIL"
+    PORT: process.env.PORT,
+    DB_HOST: process.env.DB_HOST_MYSQL,
+    DB_USER: process.env.DB_USER_MYSQL,
+    DB_PASS: process.env.DB_PASS_MYSQL,
+    DB_DATABASE: process.env.DB_DATABASE_MYSQL,
+    SECRET_JWT: process.env.SECRET_JWT || ""
 };
